0233266c6fc5940070a9148d3ef818824436d576
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-01-23 18:28:43 -0800

    Preparing for file update
    Have everything implemented except for the committing the new count part

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-console.log("yay its set up correctly");
+const GET_URL = "https://raw.githubusercontent.com/LordOfDeadbush/counter-website/main/count.txt"
 
-fetch("count.txt").then((data) => console.log(data));
+const octokit = new Octokit({
+    auth: 'YOUR-TOKEN'
+})
+
+async function getCount() {
+    count = 0;
+    fetch(GET_URL)
+        .then((data) => data.text().then((data) => count = parseInt(data)));
+        return count;
+}
+
+async function displayCount() {document.getElementById("count").innerHTML = await getCount();}
+
+async function tally() {
+    // get the initial count and add 1
+    count = await getCount() + 1;
+
+    // display the count
+    document.getElementById("count").innerHTML = count;
+
+    // TODO deploy a commit to github to increase count by 1
+
+}
+
+tally();
